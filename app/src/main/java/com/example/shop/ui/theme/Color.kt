5f52b1d0c3cb901fe1dc7044afb7fe0555dc2229
violaderyabina <fomicheva@zamt.local>
2026-04-29 12:34:36 +0300

    Custom theme

--- app/src/main/java/com/example/shop/ui/theme/Color.kt
+++ app/src/main/java/com/example/shop/ui/theme/Color.kt
@@ -1,11 +1,34 @@
 package com.example.shop.ui.theme
 
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Red = Color(0xFFF87265)
+val Accent = Color(0xFF48B2E7)
+val Disable = Color(0xFF2B6B8B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val SubTextLight = Color(0xFFD8D8D8)
+val Background = Color(0xFFF7F7F9)
+val Block = Color(0xFFFFFFFF)
+
+val Text = Color(0xFF2B2B2B)
+val Hint = Color(0xFF6A6A6A)
+val SubTextDark = Color(0xFF707B81)
+
+@Immutable
+data class CustomColors(
+    val red: Color = Red,
+    val accent: Color = Accent,
+    val disable: Color = Disable,
+    val subTextLight: Color = SubTextLight,
+    val background: Color = Background,
+    val block: Color = Block,
+    val text: Color = Text,
+    val hint: Color = Hint,
+    val subTextDark: Color = SubTextDark
+)
+
+val LocalCustomColors = staticCompositionLocalOf{
+    CustomColors()
+}
--- app/src/main/java/com/example/shop/ui/theme/Type.kt
+++ app/src/main/java/com/example/shop/ui/theme/Type.kt
@@ -1,34 +1,141 @@
 package com.example.shop.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.shop.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+val type = FontFamily(
+    Font(R.font.raleway, weight = FontWeight.Normal),
+    Font(R.font.raleway_bold, weight = FontWeight.Bold),
+    Font(R.font.raleway_medium, weight = FontWeight.Medium),
+    Font(R.font.raleway_semibold, weight = FontWeight.SemiBold),
+    Font(R.font.raleway_extrabold, weight = FontWeight.ExtraBold),
     )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+@Immutable
+data class CustomTypography(
+    val HeadingRegular34: TextStyle =
+        TextStyle(
+            fontSize = 34.sp,
+            lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+    val HeadingRegular32: TextStyle=
+        TextStyle(
+            fontSize = 32.sp,
+            //lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+
+    val HeadingBold30: TextStyle =
+        TextStyle(
+            fontSize = 30.sp,
+           // lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Bold
+        ),
+    val HeadingRegular26: TextStyle=
+        TextStyle(
+            fontSize = 26.sp,
+            //lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+
+    val HeadingRegular20: TextStyle =
+        TextStyle(
+            fontSize = 20.sp,
+            lineHeight = 28.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+    val HeadingSemiBold16: TextStyle=
+        TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.SemiBold
+        ),
+
+
+    val SubtitleRegular16: TextStyle =
+        TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+    val BodyRegular24: TextStyle=
+        TextStyle(
+            fontSize = 24.sp,
+            //lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+
+    val BodyRegular20: TextStyle =
+        TextStyle(
+            fontSize = 20.sp,
+            // lineHeight = 44.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+    val BodySemiBold18: TextStyle=
+        TextStyle(
+            fontSize = 18.sp,
+            lineHeight = 24.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.SemiBold
+        ),
+
+    val BodyMedium16: TextStyle =
+        TextStyle(
+            fontSize = 16.sp,
+            lineHeight = 20.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Medium
+        ),
+    val BodyRegular16: TextStyle=
+        TextStyle(
+            fontSize = 16.sp,
+            //lineHeight = 20.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.SemiBold
+        ),
+
+
+    val BodyMedium14: TextStyle=
+        TextStyle(
+            fontSize = 14.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Medium
+        ),
+
+    val BodyRegular14: TextStyle =
+        TextStyle(
+            fontSize = 14.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
+    val BodyRegular12: TextStyle=
+        TextStyle(
+            fontSize = 12.sp,
+            lineHeight = 16.sp,
+            letterSpacing = 0.sp,
+            fontWeight = FontWeight.Normal
+        ),
 )
+
+val LocalCustomTypography = staticCompositionLocalOf {
+    CustomTypography()
+}
