8f1c0aabdba2ca9f0b00408b476ddc7c6c46224d
violaderyabina <fomicheva@zamt.local>
2026-04-30 14:04:19 +0300

    Create myCart

---
+++ app/src/main/java/com/example/shop/presentation/components/CartBasket.kt
@@ -0,0 +1,96 @@
+package com.example.shop.presentation.components
+
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shop.R
+import com.example.shop.presentation.theme.CustomTheme
+
+@Composable
+fun CartBasket(
+    modifier: Modifier = Modifier,
+    bitmap: Int,
+    title: String,
+    price: Int
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardColors(
+            containerColor = CustomTheme.colors.block,
+            contentColor = CustomTheme.colors.text,
+            disabledContainerColor = CustomTheme.colors.block,
+            disabledContentColor = CustomTheme.colors.text,
+        )
+    ) {
+        Row(
+            modifier = Modifier.padding(top = 10.dp, start = 10.dp, bottom = 9.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(85.dp)
+                    .width(87.dp)
+                    .background(
+                        CustomTheme.colors.background,
+                        shape = RoundedCornerShape(16.dp)
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    bitmap = ImageBitmap.imageResource(bitmap),
+                    contentDescription = "",
+                    contentScale = ContentScale.FillWidth,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            Spacer(modifier = Modifier.width(30.dp))
+            Column() {
+                Text(
+                    text = title,
+                    style = CustomTheme.typography.BodyRegular16,
+                    color = CustomTheme.colors.text
+                )
+                Text(
+                    text = "₽${price}",
+                    style = CustomTheme.typography.BodyRegular16,
+                    color = CustomTheme.colors.text
+                )
+
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun CartBasketPrev() {
+    CustomTheme {
+        CartBasket(
+            bitmap = R.drawable.bot,
+            title = "Nike Club Max",
+            price = 584
+        )
+    }
+}
---
+++ app/src/main/java/com/example/shop/presentation/screens/MyCart.kt
@@ -0,0 +1,188 @@
+package com.example.shop.presentation.screens
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.shop.R
+import com.example.shop.presentation.components.CartBasket
+import com.example.shop.presentation.components.PrimaryButton
+import com.example.shop.presentation.theme.CustomTheme
+
+@Composable
+fun MyCart(modifier: Modifier = Modifier) {
+    val scroll = rememberScrollState()
+    Column() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(0.7f)
+                .background(CustomTheme.colors.background)
+        ) {
+            Column(
+                modifier = modifier
+                    .verticalScroll(scroll)
+                    .padding(horizontal = 20.dp)
+                    .fillMaxSize()
+                    .background(CustomTheme.colors.background)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    IconButton(
+                        modifier = Modifier
+                            .clip(shape = CircleShape)
+                            .background(CustomTheme.colors.block)
+                            .size(44.dp), onClick = { }
+                    ) {
+                        Icon(
+                            imageVector = ImageVector.vectorResource(R.drawable.direction_left),
+                            contentDescription = "",
+                            modifier = Modifier
+                                .size(14.dp)
+                        )
+                    }
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = "Корзина",
+                            style = CustomTheme.typography.BodyRegular16,
+                            color = CustomTheme.colors.text
+                        )
+                    }
+
+                }
+                CartBasket(
+                    bitmap = R.drawable.bot,
+                    title = "Nike Club Max",
+                    price = 584
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+                CartBasket(
+                    bitmap = R.drawable.bot,
+                    title = "Nike Club Max",
+                    price = 584
+                )
+                Spacer(modifier = Modifier.height(14.dp))
+                CartBasket(
+                    bitmap = R.drawable.bot,
+                    title = "Nike Club Max",
+                    price = 584
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .weight(0.3f)
+                .background(CustomTheme.colors.block)
+        ) {
+            Column(
+                modifier = Modifier
+                    .verticalScroll(scroll)
+                    .padding(horizontal = 20.dp)
+                    .fillMaxSize()
+            ) {
+                Spacer(modifier= Modifier.height(36.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(
+                        text = "Сумма",
+                        style = CustomTheme.typography.BodyRegular16,
+                        color = CustomTheme.colors.subTextDark
+                    )
+                    Text(
+                        text = "₽753.95",
+                        style = CustomTheme.typography.BodyRegular16,
+                        color = CustomTheme.colors.text
+                    )
+                }
+                Spacer(modifier= Modifier.height(11.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(
+                        text = "Доставка",
+                        style = CustomTheme.typography.BodyRegular16,
+                        color = CustomTheme.colors.subTextDark
+                    )
+                    Text(
+                        text = "₽60.20",
+                        style = CustomTheme.typography.BodyRegular16,
+                        color = CustomTheme.colors.text
+                    )
+                }
+                Spacer(modifier= Modifier.height(34.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(end = 20.dp),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                ) {
+                    Text(
+                        text = "Итого",
+                        style = CustomTheme.typography.BodyRegular16,
+                        color = CustomTheme.colors.text
+                    )
+                    Text(
+                        text = "₽814.15",
+                        style = CustomTheme.typography.BodyRegular16,
+                        color = CustomTheme.colors.accent
+                    )
+                }
+                Spacer(modifier= Modifier.height(33.dp))
+                PrimaryButton(
+                    onClick = {},
+                    text = "Оформить заказ",
+                    enabled = true,
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier= Modifier.height(32.dp))
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+private fun MyCartPrev() {
+    CustomTheme {
+        MyCart()
+    }
+}
